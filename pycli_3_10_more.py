419166840703324d45a9d55ff7e566b4a7d64ccf
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2024-09-28 19:46:31 +0200

    Update pycli_3_10_more.py

--- pycli_3_10_more.py
+++ pycli_3_10_more.py
@@ -196,7 +196,7 @@ class CLI:
 
     def change_directory(self, path : str = home) -> None:
         "Allows you to change the location of the terminal in your files."
-        npath = os.apath.join(self.path, path)
+        npath = os.path.join(self.path, path)
         if os.path.isdir(npath):
             path = os.path.normpath(npath)
         else:
